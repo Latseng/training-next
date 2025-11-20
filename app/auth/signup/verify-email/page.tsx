a976c1acf179c9c6975a3aa121855eec5ca292d0
Lala Tseng <z162614@gmail.com>
2025-11-20 21:30:42 +0800

    fix: add suspense boundary

--- app/auth/signup/verify-email/page.tsx
+++ app/auth/signup/verify-email/page.tsx
@@ -1,9 +1,19 @@
 import VerifyEmailClient from "@/components/VerifyEmailClient";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 export default function VerifyEmailPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-muted/40 px-4">
-      <VerifyEmailClient />
+      <Suspense
+        fallback={
+          <div className="my-20 mx-20 flex items-center justify-center p-8 rounded-xl shadow-lg">
+            <Loader2 className="h-8 w-8 animate-spin text-primary" />
+          </div>
+        }
+      >
+        <VerifyEmailClient />
+      </Suspense>
     </div>
   );
 }
